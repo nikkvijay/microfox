316bb339d4261153849aebd1e37fa0d827cccbf0
VISHWAJ33T <vishwajeety14122@gmail.com>
2025-05-15 18:27:37 +0530

    feat: add constants for serverless deployment configuration

---
+++ scripts/src/serverless/deploy/constants.ts
@@ -0,0 +1,11 @@
+import path from "path";
+
+const BASE_SERVER_URL = "https://staging.microfox.app";
+const MODE = "staging";
+const PACKAGES_PATH = path.join(__dirname, '..', '..', '..', '..', 'packages');
+
+export {
+    BASE_SERVER_URL,
+    MODE,
+    PACKAGES_PATH,
+} 
